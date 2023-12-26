bfd3703d30bfea9f09a8c52fe5bd8e7c35c05224
DSunilDev <sunilkumardeveops@gmail.com>
2023-12-26 17:07:26 +0530

    i

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const path=require('path')
+
+const app=express();
+
+app.use(express.urlencoded({extended:true}))
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.use(express.static('dark'));
+app.use(express.static('assets'));
+app.use(express.static('demo'));
+app.use(express.static('help-docs'));
+app.use(express.static('light'));
+app.use(express.static('views'));
+
+
+app.get('/',function(req,res){
+    res.render('dark')
+})
+
+app.listen(500)
